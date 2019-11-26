bdc594e8cd56cfada04d8ef035434d66e05c4e19
Alex <a.plyushchay@gmail.com>
2019-11-26 11:53:33 -0500

    Setup working tests configs

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,14 @@
 module.exports = {
+  preset: 'ts-jest',
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    setupFilesAfterEnv: ['<rootDir>/test/jest.setup.js'],
+    transform: {
+      '^.+\\.tsx$': 'ts-jest',
+    },
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'js', 'json', 'node', 'tsx'],
+    setupFilesAfterEnv: ['<rootDir>/test/jest.setup.js'], // doesn't work =[
   },
 };
--- test/QueryBuilder.test.tsx
+++ test/QueryBuilder.test.tsx
@@ -1,7 +1,38 @@
-import { shallow, ShallowWrapper } from 'enzyme';
+import { configure, shallow, ShallowWrapper } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import * as React from 'react';
 import QueryBuilder from '../src/index';
+import {
+  ClassNames,
+  IRuleGroup,
+  RuleElements,
+  RuleGroupElements,
+} from '../src/models';
 import { IQueryBuilderProps } from '../src/QueryBuilder';
+import {
+  createInitialClassNames,
+  createInitialQuery,
+  createInitialRuleElements,
+  createInitialRuleGroupElements,
+} from '../src/utils';
+
+configure({ adapter: new Adapter() });
+
+jest.mock('../src/utils', () => ({
+  createInitialClassNames: jest.fn((_: ClassNames) => ({
+    queryBuilder: 'classNames',
+  })),
+  createInitialRuleElements: jest.fn((_1: any[], _2: RuleElements) => 'rule'),
+  createInitialRuleGroupElements: jest.fn(
+    (_: RuleGroupElements) => 'ruleGroups',
+  ),
+  createInitialQuery: jest.fn((_1: RuleGroupElements, _2: RuleElements) => ({
+    id: 1,
+    combinator: 'and',
+    conditions: [],
+  })),
+  typeCheck: jest.fn(),
+}));
 
 describe('it', () => {
   let props: IQueryBuilderProps;
@@ -10,18 +41,37 @@ describe('it', () => {
   beforeEach(() => {
     props = {
       columns: ['apple', 'water', 'dog'],
+      onQueryChange: jest.fn(),
     };
-
     wrapper = shallow(<QueryBuilder {...props} />);
   });
 
-  it('renders without crashing', () => {
-    wrapper.exists();
+  describe('renders', () => {
+    it('without crashing', () => {
+      wrapper.exists();
+    });
+    it('ruleGroup', () => {
+      const ruleGroup = wrapper.find('RuleGroup');
+      expect(ruleGroup.exists()).toBeTruthy();
+    });
+    it('ruleGroup with query', () => {
+      const query: IRuleGroup = wrapper.state('query');
+      expect(query.id).toBe(1);
+    });
   });
 
-  // describe('creates default', () => {
-  //   it('classNames', () => {
-  //     wrapper.props;
-  //   });
-  // });
+  describe('creates', () => {
+    it('classNames', () => {
+      expect(createInitialClassNames).toHaveBeenCalled();
+    });
+    it('rule elements', () => {
+      expect(createInitialRuleElements).toHaveBeenCalled();
+    });
+    it('rule group elements', () => {
+      expect(createInitialRuleGroupElements).toHaveBeenCalled();
+    });
+    it('initialized state', () => {
+      expect(createInitialQuery).toHaveBeenCalled();
+    });
+  });
 });
