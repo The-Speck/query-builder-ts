241a1f3561b232f2a01e6a8b8adcf6582d0d5fe9
Alex <a.plyushchay@gmail.com>
2019-11-26 21:19:10 -0500

    finish find utils

--- test/utils/findCondition.test.tsx
+++ test/utils/findCondition.test.tsx
@@ -41,8 +41,67 @@ describe('it', () => {
           conditions: ['condition1', 'condition2'],
         },
       ]);
-      const result = find.findCondition('1', query);
+      const result = find.findCondition('condition2', query);
       expect(result).toBe('condition2');
+      spy.mockRestore();
+    });
+
+    it('condition exist', () => {
+      const spy = jest.spyOn(find, 'findConditionIdxAndParentGroup');
+      spy.mockReturnValue([
+        null,
+        {
+          id: '1',
+          combinator: 'and',
+          conditions: ['condition1', 'condition2'],
+        },
+      ]);
+      const result = find.findCondition('1', query);
+      expect((result as IRuleGroup).id).toBe('1');
+      spy.mockRestore();
+    });
+
+    it('none exist', () => {
+      const spy = jest.spyOn(find, 'findConditionIdxAndParentGroup');
+      spy.mockReturnValue([null, null]);
+      const result = find.findCondition('1', query);
+      expect(result).toBeNull();
+      spy.mockRestore();
+    });
+  });
+
+  describe('finds', () => {
+    it('condition index and parent group', () => {
+      const [idx, parentGroup] = find.findConditionIdxAndParentGroup(
+        '2',
+        query,
+      );
+      if (idx !== null && parentGroup) {
+        const condition = parentGroup.conditions[idx];
+        expect(condition.id).toBe('2');
+      } else {
+        throw new Error('error running findConditionIdxAndParentGroup test');
+      }
+    });
+    it('parent group', () => {
+      const [idx, parentGroup] = find.findConditionIdxAndParentGroup(
+        '0',
+        query,
+      );
+      expect(idx).toBeNull();
+      if (parentGroup) {
+        expect(parentGroup.id).toBe('0');
+      } else {
+        throw new Error('error running findConditionIdxAndParentGroup test');
+      }
+    });
+    it('nothing', () => {
+      const [idx, parentGroup] = find.findConditionIdxAndParentGroup(
+        '-1',
+        query,
+      );
+      expect(idx).toBeNull();
+      expect(parentGroup).toBeNull();
     });
   });
 });
