80a40bf6c2d4b7e51d848f7aa1be3448d0c21409
Alex <a.plyushchay@gmail.com>
2020-01-16 20:26:15 -0500

    Fix typeCheck array bug

--- src/utils/typeCheck.tsx
+++ src/utils/typeCheck.tsx
@@ -3,7 +3,13 @@ export const typeCheck = (value: any, ...args: any[]): any => {
     case 'function':
       return value(...args);
     case 'object':
-      return args ? value[args[0]] : value;
+      if (Array.isArray(value)) {
+        return value;
+      } else if (args.length && value.hasOwnProperty(args[0])) {
+        return value[args[0]];
+      } else {
+        return value;
+      }
     default:
       return value;
   }
