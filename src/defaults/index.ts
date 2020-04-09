2a6e806f69eb0e4faaee92a6f2e605710560b448
Alex <a.plyushchay@gmail.com>
2020-04-09 18:03:07 -0400

    export default operators and combinators

--- src/defaults/index.ts
+++ src/defaults/index.ts
@@ -1,10 +1,14 @@
 import CLASSNAMES from './elementOptions/classNames';
+import COMBINATORS from './elementOptions/combinators';
+import OPERATORS from './elementOptions/operators';
 import RULE from './ruleElements';
 import RULE_GROUP from './ruleGroupElements';
 
 export const Defaults = {
   RULE,
   RULE_GROUP,
+  OPERATORS,
+  COMBINATORS,
   CLASSNAMES,
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,5 @@ import QueryBuilder from './QueryBuilder';
 export default QueryBuilder;
 
 export * from './controls';
-export * from './defaults/ruleElements';
-export * from './defaults/ruleGroupElements';
+export * from './defaults';
 export * from './models';
